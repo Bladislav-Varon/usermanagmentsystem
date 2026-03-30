48678cbe0ded62c7dec5e33f2a314ea6b2bb4f04
ISPO0123168 Ренк Андрей Александрович <ISPO0123168@sibupk.su>
2026-03-30 18:39:19 +0700

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagementSystem
+{
+    public class UserManager
+    {
+        // Создаём список для хранения пользователей
+        private List<string> _users = new List<string>();
+
+        // Метод для добавления пользователя
+        public void AddUser(string username)
+        {
+            _users.Add(username);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UserManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Тестовый код для проверки функционала (из ветки feature/user-removal)
+            var manager = new UserManager();
+            manager.AddUser("test");
+            Console.WriteLine("Добавлен пользователь 'test'");
+
+            bool isRemoved = manager.RemoveUser("test");
+            Console.WriteLine($"Результат удаления пользователя 'test': {isRemoved}"); // Должно вывести True
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
